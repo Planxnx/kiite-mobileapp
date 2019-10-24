1b5f32d4a738928401bdbeef77d7af865ce405c9
Planxnx <60010465@kmitl.ac.th>
2019-10-24 22:17:47 +0700

    connect AsyncStorage

--- views/MainScreen.js
+++ views/MainScreen.js
@@ -2,6 +2,24 @@ import React from 'react';
 import { AsyncStorage,StyleSheet, Text, View ,Button } from 'react-native';
 
 export default class MainScreen extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            username: "token",
+            passwordInput: "",
+            isLoading: false,
+        };
+    }
+
+    componentDidMount() {
+        AsyncStorage.multiGet(['username','token','role']).then((data) => {
+          this.setState({
+            username:data[0][1],
+            token:data[1][1],
+            role:data[2][1]
+          });
+        });
+    }
 
     static navigationOptions = {
         header: null,
@@ -13,10 +31,14 @@ export default class MainScreen extends React.Component {
     }
 
     render(){
+        const { username,role,token } = this.state;
         return (
             <View style={styles.container}>
                 <Text style={styles.text}>
                     Kiite
+                </Text>
+                <Text style={styles.text}>
+                    username: {username}
                 </Text>
                 <Button title="Setting" onPress={this._settingApp} />
             </View>
