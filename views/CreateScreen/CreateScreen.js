4979719e4c4de3de12d6f4c870749f72ce20d3d5
Planxnx <60010465@kmitl.ac.th>
2019-10-27 18:48:30 +0700

    add terms of service

--- views/CreateScreen/CreateScreen.js
+++ views/CreateScreen/CreateScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { ActivityIndicator,AsyncStorage, StyleSheet, Text, View ,Button ,Image,TouchableHighlight ,TouchableOpacity ,Alert } from 'react-native';
+import { ActivityIndicator,AsyncStorage, StyleSheet, Text, View ,KeyboardAvoidingView ,Image,TouchableHighlight ,TouchableOpacity ,Alert } from 'react-native';
 import { TextInput } from 'react-native-gesture-handler';
 import { vw, vh, vmin, vmax } from 'react-native-expo-viewport-units';
+import { CheckBox } from 'react-native-elements'
 
 export default class CreateScreen extends React.Component {
     constructor() {
@@ -11,6 +12,7 @@ export default class CreateScreen extends React.Component {
             passwordInput: "",
             repasswordInput: "",
             emailInput: "",
+            termCheck: false,
             isLoading: false,
         };
     }
@@ -27,6 +29,38 @@ export default class CreateScreen extends React.Component {
 
     gotoMain = () => {
         this.props.navigation.navigate('Main')
+    }
+
+    acceptTerms = () =>{
+        Alert.alert(
+            'Terms of service',
+            'To use Kiite, you must accept the Terms of service',
+            [
+              {text: 'Accept',
+              onPress: () => {
+                this.setState({
+                    termCheck: true
+                });
+              }},
+              {
+                text: 'Not now',
+                onPress: () => {
+                    this.setState({
+                        termCheck: false
+                    });
+                },
+                style: 'cancel',
+              },
+            ],
+          )
+        if (!this.state.termCheck) {
+            this.setState({
+                isLoading: false
+            });
+            return
+        }else{
+            this.signUp()
+        }
     }
 
     signUp = async () => {
@@ -54,6 +88,20 @@ export default class CreateScreen extends React.Component {
             Alert.alert(
                 '',
                 "The Username field must be at least 6 characters or less than 30 characters",
+                [
+                  {text: 'OK'},
+                ],
+                {cancelable: false},
+            )
+            return
+        }
+        if (!this.emailValidate(this.state.emailInput)){
+            this.setState({
+                isLoading: false
+            });
+            Alert.alert(
+                '',
+                "The format of the email address isn't correct",
                 [
                   {text: 'OK'},
                 ],
@@ -146,11 +194,22 @@ export default class CreateScreen extends React.Component {
         })   
     }
 
+    emailValidate = (text) => {
+        let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ;
+        if(reg.test(text) === false){
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
     render(){
         const { isLoading,warnMessage } = this.state;
+        const inputAccessoryViewID = "uniqueID";
 
         return (
-        <View style={styles.container}>
+        <KeyboardAvoidingView  style={styles.container}>
             
             <View style={styles.createForm} >
                 <Text style={styles.createHeader}>
@@ -166,6 +225,7 @@ export default class CreateScreen extends React.Component {
                         placeholder="Username"
                         autoCompleteType="username"
                         autoCapitalize = "none"
+                        maxLength = "30"
                         onChangeText={usernameInput => {
                             this.setState({ usernameInput });
                         }}
@@ -182,6 +242,7 @@ export default class CreateScreen extends React.Component {
                         placeholder="E-mail"
                         autoCompleteType="email"
                         autoCapitalize = "none"
+                        keyboardType = "email-address"
                         onChangeText={emailInput => {
                             this.setState({ emailInput });
                         }}
@@ -198,6 +259,7 @@ export default class CreateScreen extends React.Component {
                         placeholder="Password"
                         autoCompleteType="password"
                         autoCapitalize = "none"
+                        maxLength = "24"
                         secureTextEntry = {true}
                         onChangeText={passwordInput => {
                             this.setState({ passwordInput });
@@ -215,6 +277,7 @@ export default class CreateScreen extends React.Component {
                         placeholder="Confirm Password"
                         autoCompleteType="password"
                         autoCapitalize = "none"
+                        maxLength = "24"
                         secureTextEntry = {true}
                         onChangeText={repasswordInput => {
                             this.setState({ repasswordInput });
@@ -222,8 +285,18 @@ export default class CreateScreen extends React.Component {
                         value={this.state.repasswordInput}
                     />
                 </View>
+                <TouchableOpacity>
+                    <Text 
+                        style={styles.linkText}
+                        // onPress={()=>{
+                        //     this.props.navigation.navigate('Create')
+                        // }}
+                    >
+                            Terms of service
+                    </Text>
+                </TouchableOpacity>
                 { isLoading ? <TouchableOpacity disabled={true}><View style={styles.buttontext}><ActivityIndicator/></View></TouchableOpacity> : 
-                    <TouchableOpacity onPress={this.signUp}><View style={styles.buttontext} ><Text style={{textAlignVertical:'center',fontSize:vh(1.799)}}>Login</Text></View></TouchableOpacity>
+                    <TouchableOpacity onPress={this.acceptTerms}><View style={styles.buttontext} ><Text style={{textAlignVertical:'center',fontSize:vh(1.799)}}>Create</Text></View></TouchableOpacity>
                 }
              </View>
              <View style={styles.footer} >
@@ -239,7 +312,7 @@ export default class CreateScreen extends React.Component {
                     </Text>
                 </TouchableOpacity>
              </View>
-        </View>
+        </KeyboardAvoidingView >
         )
     }
 }
@@ -305,7 +378,7 @@ const styles = StyleSheet.create({
         marginTop:vh(14.791),
         flexDirection: 'row'
     },
-    createText:{
+    linkText:{
         fontSize: vh(1.799),
         color: '#F8F8F8',
         fontWeight:"bold",
