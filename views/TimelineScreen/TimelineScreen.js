313caab34f384a9b8b046cc9fb820a6a1ce738ea
Planxnx <60010465@kmitl.ac.th>
2019-10-29 04:36:28 +0700

    add fetch api

--- views/TimelineScreen/TimelineScreen.js
+++ views/TimelineScreen/TimelineScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AsyncStorage,StyleSheet,Text, Button, View ,ScrollView  ,TouchableOpacity,Image } from 'react-native';
+import { AsyncStorage,StyleSheet,Text, ActivityIndicator, View ,ScrollView  ,TouchableOpacity,Image } from 'react-native';
 import { vw, vh, vmin, vmax } from 'react-native-expo-viewport-units';
 import TimelineStatus from  './components/timelineStatus'
 import OverallStatus from  './components/overallStatus'
@@ -11,41 +11,10 @@ export default class TimelineScreen extends React.Component {
             username: "token",
             passwordInput: "",
             isLoading: false,
-            exmapleResp: {
-                status:200,
-                data:[
-                    {
-                        text: "pretium cursus mattis molestie a iaculis at erat pellentesque adipiscing commodo elit at imperdiet dui accumsan sit amet nulla facilisi morbi tempus iaculis urna id volutpat lacus laoreet non curabitur gravida arcu ac tortor dignissim convallis aenean et tortor at",
-                        mood: "pos"
-                    },{
-                        text: "tempor orci dapibus ultrices in iaculis nunc sed augue lacus viverra vitae congue eu consequat ac felis donec et odio pellentesque diam volutpat commodo",
-                        mood: "neg"
-                    },{
-                        text: "viverra ipsum nunc aliquet bibendum enim facilisis gravida neque convallis a cras semper auctor neque vitae tempus quam pellentesque nec nam aliquam sem et tortor consequat id porta nibh venenatis cras sed felis eget velit aliquet sagittis id consectetur purus ut faucibus pulvinar elementum integer enim neque volutpat ac tincidunt vitae semper quis lectus nulla at volutpat diam ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet proin fermentum leo vel orci porta non pulvinar neque laoreet suspendisse",
-                        mood: "neg"
-                    },{
-                        text: "adipiscing commodo elit at imperdiet dui accumsan sit amet nulla facilisi",
-                        mood: "neg"
-                    },{
-                        text: "felis donec et odio pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc sed velit dignissim sodales ut eu sem integer vitae justo eget magna fermentum iaculis eu non diam phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet enim tortor at auctor urna nunc id cursus metus aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget duis at tellus at urna condimentum mattis pellentesque id nibh tortor id aliquet lectus proin nibh nisl condimentum id venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas sed tempus urna et pharetra pharetra massa massa ultricies mi quis hendrerit dolor magna eget est lorem ipsum dolor sit amet",
-                        mood: "pos"
-                    },{
-                        text: "non enim praesent eget nulla facilisi etiam dignissim diam quis enim lobortis sagittis eu volutpat odio facilisis mauris sit amet massa vitae tortor condimentum lacinia quis vel eros donec ac odio tempor orci dapibus ultrices in iaculis nunc sed augue lacus viverra vitae congue eu consequat ac felis donec et odio pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc sed velit dignissim sodales ut",
-                        mood: "neg"
-                    },{
-                        text: "felis donec et odio pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc sed velit dignissim sodales ut eu sem integer vitae justo eget magna fermentum iaculis eu non diam phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet enim tortor at auctor urna nunc id cursus metus aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget duis at tellus at urna condimentum mattis pellentesque id nibh tortor id aliquet lectus proin nibh nisl condimentum id venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas sed tempus urna et pharetra pharetra massa massa ultricies mi quis hendrerit dolor magna eget est lorem ipsum dolor sit amet",
-                        mood: "pos"
-                    },{
-                        text: "felis donec et odio pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc sed velit dignissim sodales ut eu sem integer vitae justo eget magna fermentum iaculis eu non diam phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet enim tortor at auctor urna nunc id cursus metus aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget duis at tellus at urna condimentum mattis pellentesque id nibh tortor id aliquet lectus proin nibh nisl condimentum id venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas sed tempus urna et pharetra pharetra massa massa ultricies mi quis hendrerit dolor magna eget est lorem ipsum dolor sit amet",
-                        mood: "neg"
-                    },{
-                        text: "felis donec et odio pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur vitae nunc sed velit dignissim sodales ut eu sem integer vitae justo eget magna fermentum iaculis eu non diam phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet enim tortor at auctor urna nunc id cursus metus aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a scelerisque purus semper eget duis at tellus at urna condimentum mattis pellentesque id nibh tortor id aliquet lectus proin nibh nisl condimentum id venenatis a condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas sed tempus urna et pharetra pharetra massa massa ultricies mi quis hendrerit dolor magna eget est lorem ipsum dolor sit amet",
-                        mood: "neg"
-                    }
-                ]     
+            respData:{
+                data:[{mood:"pos"}]
             }
         }
-        this._findMoodCount()
     }
 
     static navigationOptions = ({ navigation }) => ({
@@ -72,6 +41,30 @@ export default class TimelineScreen extends React.Component {
             role:data[2][1]
           });
         });
+        this._fetchStatus()
+    }
+
+    _fetchStatus = () =>{
+        this.setState({
+            isLoading: true
+        });
+        fetch('http://5db18f0de9751d0014ccf91a.mockapi.io/api/vtest/timeline/', {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            }
+        })
+        .then(response=>response.json())
+        .then((responseJson) => {
+            this.setState({
+                respData: responseJson,
+            }); 
+            this._findMoodCount()
+            this.setState({
+                isLoading: false
+            });                
+        })
     }
 
     _signOutAsync = async () => {
@@ -86,8 +79,8 @@ export default class TimelineScreen extends React.Component {
     _findMoodCount = async () => {
         let posCount = 0
         let negCount = 0
-        for (let i = 0; i < this.state.exmapleResp.data.length; i++){
-            if (this.state.exmapleResp.data[i].mood == "pos"){
+        for (let i = 0; i < this.state.respData.data.length; i++){
+            if (this.state.respData.data[i].mood == "pos"){
                 posCount +=1
             }else{
                 negCount +=1
@@ -98,19 +91,27 @@ export default class TimelineScreen extends React.Component {
     }
 
     render(){
+        if(this.state.isLoading){
+            return (
+                <View style={styles.container}>
+                    <ActivityIndicator/>
+                </View>       
+            );
+        } else{
+            let TimelinseStatuses =  this.state.respData.data.map((data,key)=>{
+                return <TimelineStatus key = {key} text={data.text} />
+            })
+            return (
+                <View style={styles.container}>
+                    <OverallStatus/>
+                    <ScrollView style={{flex: 1}} >
+                        {TimelinseStatuses}
+                    </ScrollView>
+                </View>       
+            );
+        }
 
-        const TimelinseStatuses =  this.state.exmapleResp.data.map((data,key)=>{
-            return <TimelineStatus key = {key} text={data.text} />
-        })
-
-        return (
-            <View style={styles.container}>
-                <OverallStatus/>
-                <ScrollView style={{flex: 1}} >
-                    {TimelinseStatuses}
-                </ScrollView>
-            </View>       
-        );
+        
     }
     
 }
