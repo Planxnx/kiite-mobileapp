5b7603f480d086e94237b0f6d01a15c1eaf9fd80
Planxnx <60010465@kmitl.ac.th>
2019-11-15 01:01:11 +0700

    update bottom navbar

--- Navigator/MainNavigator.js
+++ Navigator/MainNavigator.js
@@ -1,14 +1,49 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/Ionicons'
 import { createAppContainer } from 'react-navigation';
-import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { vw, vh } from 'react-native-expo-viewport-units';
+import { createBottomTabNavigator,BottomTabBar  } from 'react-navigation-tabs';
 
 import SettingStack from './SettingStack' 
 import TimelineStack from './TimelineStack'
 import ChatStack from './ChatStack'
 
+const TabBarComponent = props => <BottomTabBar {...props} />;
+
 const TabNavigator = createBottomTabNavigator({
-    Timeline: { screen: TimelineStack },
-    Chat: { screen: ChatStack },
-    Settings: { screen: SettingStack },
+    Timeline: { 
+        screen: TimelineStack,
+        navigationOptions: {
+            tabBarIcon: ({tintColor, activeTintColor}) => (
+                <Icon name="ios-paper" size={vh(5.2473763)} color={tintColor} />
+            )
+        }
+    },
+    Chat: { 
+        screen: ChatStack,
+        navigationOptions: {
+            tabBarIcon: ({tintColor, activeTintColor}) => (
+                <Icon name="ios-chatboxes" size={vh(5.2473763)} color={tintColor} />
+            )
+        }
+     },
+    Settings: { 
+        screen: SettingStack,
+        navigationOptions: {
+            tabBarIcon: ({tintColor, activeTintColor}) => (
+                <Icon name="ios-settings" size={vh(5.2473763)} color={tintColor} />
+            )
+        }
+     },
+},
+{
+  tabBarOptions :{
+    showLabel: false,
+    activeTintColor : "#EA5757",
+    style:{
+        backgroundColor: "rgba(47, 196, 178,0.3)"
+    }
+  }
 });
 
 export default createAppContainer(TabNavigator);
