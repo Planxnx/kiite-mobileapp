e593d812f8f8dbe7740fa984a959749d7e3287c2
Planxnx <60010465@kmitl.ac.th>
2019-11-18 04:50:34 +0700

    add route handle

--- Navigator/AuthNavigator.js
+++ Navigator/AuthNavigator.js
@@ -3,10 +3,32 @@ import {createStackNavigator} from 'react-navigation-stack';
 
 import LoginScreen from '../views/LoginScreen/LoginScreen'
 import CreateScreen from '../views/CreateScreen/CreateScreen'
+import AboutScreen from '../views/AboutusScreen/AboutScreen'
+import PrivacyScreen from '../views/PrivacyScreen/PrivacyScreen'
 
 const AppStack = createStackNavigator({
     Login: LoginScreen,
-    Create: CreateScreen
+    Create: CreateScreen,
+    Aboutus: {
+        screen: AboutScreen,
+        navigationOptions : {
+            headerTintColor: '#262626',
+            headerTransparent: false,
+            headerStyle: {
+                backgroundColor: '#2FC4B2',
+            }
+        }
+    },
+    PrivacyPolicy: {
+        screen: PrivacyScreen,
+        navigationOptions : {
+            headerTintColor: '#262626',
+            headerTransparent: false,
+            headerStyle: {
+                backgroundColor: '#2FC4B2',
+            }
+        }
+    }
 });
 
 export default AppStack;
